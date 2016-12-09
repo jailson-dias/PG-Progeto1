864a2371a7d6adf775d1b10ba1f1172f998d2457
Jailson Dias <jcd2@cin.ufpe.br>
2016-12-09 01:53:57 -0300

    opengl

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import _thread as thread
 
 from hermite_bezier import main_hermite
-from ui import start
+# from ui import start
+from mundo import main
 
 try:
     thread.start_new_thread(main_hermite, ())
-    start()
+    main()
 except KeyboardInterrupt:
     thread.exit()
+
+
+# while(True):
+#     pass
--- mundo.py
+++ mundo.py
@@ -1,5 +1,10 @@
 
 
+# tamanho da tela
+
+height, width = 400, 600
+
+pontos_tela = []
 pontos = []
 triangulos = []
 luz = 0
@@ -36,8 +41,13 @@ class Ponto3D(object):
     def __mul__(self, a):
         return Ponto3D(self.x*a,self.y*a,self.z*a)
 
+    def __neg__(self):
+        return self*-1
+
     def normalizado(self):
         tam = self.norma()
+        if (eq(tam,0)):
+            tam = 1
         self.x = self.x/tam
         self.y = self.y/tam
         self.z = self.z/tam
@@ -107,21 +117,39 @@ class RGB(object):
     def __add__(self, cor):
         return RGB(min(self.r+cor.r,255),min(self.g+cor.g,255),min(self.b+cor.b,255))
 
+    def __str__(self):
+        return "("+str(self.r)+", "+str(self.g)+", "+str(self.b)+")"
+
 # utilizado para calcular a formula (I = Ia.ka + Ip*Op.kd.(N.L) + Ipm.ks.(R.V)^q)
 def get_cor(i):
     global camera, luz, pontos
     ponto = pontos[i]
     ia = luz.ia*luz.ka
+    # print (ia, "ia")
     l = (luz.pl-ponto.p)
+    # print (l,"l")
     l.normalizado()
-    ponto.normal.normalizado()
-    id = (luz.od%luz.il)*luz.kd*(ponto.normal.prod_escalar(l))
-    v = (camera.c - ponto.p)
-    v.normalizado()
-    r = (ponto.normal*2)*(ponto.normal.prod_escalar(l)) - l
-    r.normalizado()
-    ie = (luz.il)*luz.ks*(pow(r.prod_escalar(v),luz.n))
+    normal = ponto.normal
+    normal.normalizado()
+    id = RGB(0,0,0)
+    ie = RGB(0,0,0)
+    if (normal.prod_escalar(l)>=0):
+        id = (luz.od%luz.il)*luz.kd*(normal.prod_escalar(l))
+        # print (id, "id")
+        v = (camera.c - ponto.p)
+        v.normalizado()
+        if (normal.prod_escalar(v)<0):
+            normal = -normal
+        # print (v, "v")
+        r = (normal*2)*(normal.prod_escalar(l)) - l
+        r.normalizado()
+        if (v.prod_escalar(r)>=0):
+            # print (r,"r")
+            ie = (luz.il)*luz.ks*(pow(r.prod_escalar(v),luz.n))
+            # print (ie, "ie")
+    # print (ia, id, ie, "separado")
     pontos[i].cor = ia + id + ie
+    # print (pontos[i].cor)
 
 def ler_objeto(path):
     global pontos, triangulos
@@ -150,6 +178,8 @@ def ler_objeto(path):
 
     for i in range(0,len(pontos)):
         pontos[i].normal.normalizado() # normalizando a normal dos pontos
+
+    # print (len(pontos), len(triangulos), "object")
 
 def ler_luz(path):
     global luz
@@ -197,9 +227,9 @@ def cor_pontos():
     for i in range(0,len(pontos)):
         get_cor(i)
 
-def get_dados():
-    global pontos,triangulos, luz, camera
-    return (pontos, triangulos, luz, camera)
+# def get_dados():
+#     global pontos,triangulos, luz, camera
+#     return (pontos, triangulos, luz, camera)
 
 
 
@@ -214,13 +244,14 @@ def mult_matriz(u,v,n,p):
                     n.x*p.x+n.y*p.y+n.z*p.z)
 
 # utilizados para passar os pontos da base mundo para a base camera
-def pontos_camera():
+def get_pontos_camera():
     global pontos, pontos_camera, camera
-
     for i in range(0,len(pontos)):
         pontos_camera.append(mult_matriz(camera.u, camera.v,camera.n, pontos[i].p - camera.c))
 
+    # print (len(pontos_camera), "camera")
 z_buffer = []
+cores = []
 
 class Ponto2D(object):
 
@@ -255,32 +286,208 @@ class Ponto2D(object):
 
 
 def init_z_buffer():
-    global z_buffer
+    global z_buffer, cores
 
     b = []
-    for j in range(0,600):
+    c = []
+    for j in range(0,400):
         b.append(999999999)
+        c.append(RGB(255,255,255))
 
-    for i in range(0,400):
+    for i in range(0,600):
         z_buffer.append(b)
+        cores.append(c)
 
-
-pontos_tela = []
 
 def get_ponto_tela():
-    global pontos_tela, pontos, pontos_camera, camera
+    global pontos_tela, pontos, pontos_camera, camera, width,height
     for i in range(0,len(pontos_camera)):
-        pontos_tela.append(Ponto2D(int(((camera.d/camera.hx)*(pontos_camera[i].x/pontos_camera[i].z) + 1)/2),
-                                int((1 - (camera.d/camera.hy)*(pontos_camera[i].y/pontos_camera[i].z))/2)))
+        pontos_tela.append(Ponto2D(int(((camera.d/camera.hx)*(pontos_camera[i].x/pontos_camera[i].z) + 1)*width/2),
+                                int((1 - (camera.d/camera.hy)*(pontos_camera[i].y/pontos_camera[i].z))*height/2)))
+
+    # print (len(pontos_tela), "tela")
+
+class Linha(object):
+
+    def __init__(self,a,b,c,d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __div__(self,a):
+        return Linha(self.a/a,self.b/a,self.c/a,self.d/a)
+
+    def __mod__(self, a):
+        return Linha(self.a - a.a*self.a,
+                    self.b - a.b*self.a,
+                    self.c - a.c*self.a,
+                    self.d - a.d*self.a)
+
+    def __xor__(self, a):
+        return Linha(self.a,
+                    self.b - a.b*self.b,
+                    self.c - a.c*self.b,
+                    self.d - a.d*self.b)
+
+    def __add__(self, a):
+        return Linha(self.a,
+                    self.b,
+                    self.c - a.c*self.c,
+                    self.d - a.d*self.c)
+
+class Escalona(object):
+
+    def __init__(self, l1,l2,l3):
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
+
+    def esc(self):
+        l1 = self.l1/self.l1.a
+        l2 = self.l2%l1
+        l3 = self.l3%l1
+        l2 = l2/l2.b
+        l1 = l1^l2
+        l3 = l3^l2
+        l3 = l3/l3.c
+        l1 = l1+l3
+        l2 = l2 + l3
+        return (l1.d,l2.d,l3.d)
+
+
+
+from hermite_bezier import *
+
+def into(p):
+    global width, height
+    return (p.x <= width and p.x >= 0 and p.y <= height and p.y >= 0)
+
+def get_into_tela():
+    global pontos_tela, triangulos, pontos_camera,z_buffer,cores
+    for i in range(0,len(triangulos)):
+
+        # print (i, len(triangulos))
+        # print (triangulos[i].p1,triangulos[i].p2,triangulos[i].p3, "pontos")
+        p1 = pontos_tela[triangulos[i].p1]
+        p2 = pontos_tela[triangulos[i].p2]
+        p3 = pontos_tela[triangulos[i].p3]
+        if(into(p1) and into(p2) and into(p3)):
+            # p4 = Ponto2D((p1.x+p2.x+p3.x)/3.0,(p1.y+p2.y+p3.y)/3.0)
+            # p4 = p1
+            # if (p2.x>p4.x):
+            #     p4 = p2
+            # if (p3.x>p4.x):
+            #     p4 = p3
+            # a = Linha(p1.x,p2.x,p3.x,p4.x)
+            # b = Linha(p1.y,p2.y,p3.y,p4.y)
+            # c = Linha(1.0,1.0,1.0,1.0)
+            # d = Escalona(a,b,c)
+            # alfa, beta, gama = d.esc()
+            z = (pontos_camera[triangulos[i].p1].z)
+            # pontos_camera[triangulos[i].p2].z+
+            # pontos_camera[triangulos[i].p3].z)/3.0
+            # print (z_buffer[p1.x][p1.y],"zzzz")
+            # print (p1.x,p1.y)
+            # glPointSize(1)
+            # glBegin(GL_POINTS)
+            glShadeModel(GL_SMOOTH);
+            glBegin(GL_TRIANGLES);
+            # cor = pontos[triangulos[i].p1].cor
+            # glColor3f(int(cor.r),int(cor.g),int(cor.b))
+            # print (int(cor.r),int(cor.g),int(cor.b))
+            # if (z_buffer[p1.x][p1.y] > z):
+            z_buffer[p1.x][p1.y] = z
+            cor = pontos[triangulos[i].p1].cor
+            print (p1.x,p1.y,cor)
+            glColor3f(cor.r,cor.g,cor.b)
+            glVertex2f(p1.x, p1.y)
+            z = (pontos_camera[triangulos[i].p2].z)
+            # if (z_buffer[p2.x][p2.y] > z):
+            z_buffer[p2.x][p2.y] = z
+            cor = pontos[triangulos[i].p2].cor
+            print (p2.x,p2.y,cor)
+            glColor3f(cor.r,cor.g,cor.b)
+            glVertex2f(p2.x, p2.y)
+            z = (pontos_camera[triangulos[i].p3].z)
+            # if (z_buffer[p3.x][p3.y] > z):
+            z_buffer[p3.x][p3.y] = z
+            cor = pontos[triangulos[i].p3].cor
+            print (p3.x,p3.y,cor)
+            glColor3f(cor.r,cor.g,cor.b)
+            glVertex2f(p3.x, p3.y)
+
+
+            glEnd()
+
+
+
+
+
+def display():
+    global vetores, pontos, color_point, color_vector, linhas_pontos, linhas_vetores, pontos_pontos, pontos_vetores
+    global color_line_pontos,color_line_vector
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    get_into_tela()
+
+    glFlush()
+
+
+
+
+def reshape(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 600, 400, 0.0, -5.0, 5.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+# def pintar(button, state, x, y):
+#
+#     display()
 
 
 
 def main():
-    global pontos
+    # global pontos_camera
+    glutInit()
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(600, 400)
+    glutInitWindowPosition(0, 0)
+    glutCreateWindow("PG - 2016.2")
+
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glLineWidth(3.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glutDisplayFunc(display)
+    glutReshapeFunc(reshape)
+
     init()
-    ler_objeto('objeto.txt')
+    ler_objeto('entradas/Objetos/camaro.byu')
     ler_luz('luz.txt')
-    ler_camera('camera.txt')
+    ler_camera('entradas/Cameras/camaro.cfg')
+    cor_pontos()
+    # print (pontos_camera)
+
+    get_pontos_camera()
+    init_z_buffer()
+    get_ponto_tela()
+    # glutMouseFunc(pintar)
+    # print ("fim")
+    glutMainLoop()
 
 if __name__ == '__main__':
     main()
+    # p1 = Ponto2D(1.0,0.0)
+    # p2 = Ponto2D(0.0,1.0)
+    # p3 = Ponto2D(1.0,1.0)
+    # p4 = Ponto2D(1.0,1.0)
+    # a = Linha(p1.x,p2.x,p3.x,p4.x)
+    # b = Linha(p1.y,p2.y,p3.y,p4.y)
+    # c = Linha(1.0,1.0,1.0,1.0)
+    # d = Escalona(a,b,c)
+    # print (d.esc())
